6b8a08adea739151845a8a9d1866b943486d6685
Prashant Kumar <kumarprashantggn@gmail.com>
2025-08-19 22:37:40 +0530

    updated backend url

--- frontend/src/config/config.tsx
+++ frontend/src/config/config.tsx
@@ -1,2 +1,1 @@
-export const VITE_BACKEND_URL = "http://localhost:5000";
-// "https://civic-issue-reporter-application.onrender.com"
+export const VITE_BACKEND_URL = "https://fixmytown.onrender.com";
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from "vite"
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': 'https://fixmytown.onrender.com',
     },
   },
   plugins: [react(), tailwindcss()],
